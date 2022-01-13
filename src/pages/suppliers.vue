c6c74899baa8abdd5960d3adcaeb0e83f1d74ec5
spenguin10lui <spenguin10@yahoo.com.tw>
2022-01-13 12:44:31 +0800

    router加強supplier

--- src/pages/suppliers.vue
+++ src/pages/suppliers.vue
@@ -5,8 +5,25 @@
 </template>
 
 <script>
+import { AJAX_S_checkSupplierLogin } from '@/modules/server-api';
+
 export default {
-  
+  created() {
+    // 防止 router 守衛沒啟動
+    const supplierAuthToken = this.$cookies.get('_s');
+    const supplierParams = { auth_token: supplierAuthToken }
+    AJAX_S_checkSupplierLogin(supplierParams)
+    .then(res => {
+      if (res.data.success) {
+        return;
+      } else {
+        this.$router.push({ name: 'supplier-login' });
+      }
+    })
+    .catch(() => {
+      this.$router.push({ name: 'supplier-login' });
+    })
+  }
 }
 </script>
 
